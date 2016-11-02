36ddbe6dfe2ff2df1a3c8b2c1ebd46305de7b9bb
ewu-haq <quyenha2511@gmai.com>
2016-11-02 15:14:58 -0700

    add google map , in progress

--- src/MainApp.js
+++ src/MainApp.js
@@ -5,33 +5,42 @@ import {
   Text,
   View
 } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default class sharingiosnandroidproject extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>hello world</Text>
-      </View>
+        <View style ={styles.container}>
+        <MapView
+            style={styles.map}
+            region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+            }}
+        >
+       </MapView>
+     </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+   map: {
+   position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+ },
+  
 });
 
