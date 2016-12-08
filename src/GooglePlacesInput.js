1f860065650e6efb3debe8e250fe636d1d1c317a
ewu-haq <quyenha2511@gmai.com>
2016-12-08 15:24:45 -0800

    import the rest of properties

--- src/GooglePlacesInput.js
+++ src/GooglePlacesInput.js
@@ -18,12 +18,52 @@ export default class GooglePlacesInput extends Component {
     render(){
         return (
             <GooglePlacesAutocomplete
-                    placeholder={this.state.placeHolderText}>
-                    query={{
+                    placeholder={this.state.placeHolderText}
+                    
+                    minLength={2} // minimum length of text to search 
+                    autoFocus={false}
+                    listViewDisplayed='auto'    // true/false/undefined 
+                    fetchDetails={true}
+                    renderDescription={(row) => row.terms[0].value} // display street only 
+                    onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true 
+                    console.log(data);
+                    console.log(details);
+                    }}
+                    getDefaultValue={() => {
+                    return ''; // text input default value 
+                    }}
+                   query={{
                         key: 'AIzaSyA6JO5lRIYHGTLwbKOwIlJhLSLDH9alzxA',
                         language: 'en', // language of the results
                         types: '(cities)', // default: 'geocode'
                     }}
+                    styles={{
+                    description: {
+                        fontWeight: 'bold',
+                    },
+                    predefinedPlacesDescription: {
+                        color: '#1faadb',
+                    },
+                    }}
+            
+                    currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list 
+                    currentLocationLabel="Current location"
+                    nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch 
+                    GoogleReverseGeocodingQuery={{
+                    // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro 
+                    }}
+                    GooglePlacesSearchQuery={{
+                    // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search 
+                    rankby: 'distance',   
+                    types: 'food',
+                    }}
+            
+            
+                    filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities 
+            
+                    //predefinedPlaces={[homePlace, workPlace]}>
+                    >
+                    
             </GooglePlacesAutocomplete>
         )
     }
